3245c2e2c87b82079448c815d39fc9f052f62082
lixiang810 <14120445+lixiang810@users.noreply.github.com>
2022-08-21 14:29:49 +0800

    💚 Have no notification permission in CI

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -3,8 +3,9 @@ import test from 'ava'
 import { Toaster, POWERSHELL_APP_ID } from '../index'
 
 test('sync function from native code', (t) => {
+  t.is(POWERSHELL_APP_ID, '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe')
+  if (process.env.CI) return t.pass()
   const toaster = new Toaster()
   const res = toaster.noti({ text1: 'Hell', text2: 'World', text3: 'Fk windows' })
   t.true(res)
-  t.is(POWERSHELL_APP_ID, '{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\\WindowsPowerShell\\v1.0\\powershell.exe')
 })
